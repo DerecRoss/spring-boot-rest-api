9334a1dc6b859adf60067456d975a382f1826a42
DerecRoss <derec@walle.localdomain>
2026-01-13 15:25:55 -0300

    feat: Dozer Mapper and add Mapper for PersonDto

---
+++ src/main/java/com/rest_api/spring_boot_rest_api/dto/PersonDto.java
@@ -0,0 +1,83 @@
+package com.rest_api.spring_boot_rest_api.dto;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class PersonDto implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public Long id;
+
+    public String firstName;
+
+    public String lastName;
+
+    public String adress;
+
+    public String gender;
+
+    public PersonDto() {}
+
+    public PersonDto(Long id, String firstName, String lastName, String adress, String gender) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.adress = adress;
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        PersonDto person = (PersonDto) o;
+        return Objects.equals(id, person.id) && Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName) && Objects.equals(adress, person.adress) && Objects.equals(gender, person.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, adress, gender);
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
---
+++ src/main/java/com/rest_api/spring_boot_rest_api/mapper/ObjectMapper.java
@@ -0,0 +1,27 @@
+package com.rest_api.spring_boot_rest_api.mapper;
+
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ObjectMapper {
+
+    private static final Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+
+//    O -> origin object || D -> destination object || Data conversion of two values. ( Dto <-> Entity )
+    public static <O, D> D ParseObject(O originObject, Class<D> destinationObject){
+        return mapper.map(originObject, destinationObject);
+    }
+
+    public static <O, D> List<D> ParseListObjects(List<O> originObject, Class<D> destinationObject){
+
+        List<D> destinationObjects = new ArrayList<D>();
+        for (Object o : originObject){
+            destinationObjects.add(mapper.map(o, destinationObject));
+        }
+
+        return destinationObjects;
+    }
+}
