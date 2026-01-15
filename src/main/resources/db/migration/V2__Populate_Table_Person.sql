3293cbda8d644589488f250b426644fb73a8301f
DerecRoss <derec@walle.localdomain>
2026-01-15 15:40:13 -0300

    feat: add api support for migrations.

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,4 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(2, 'Springfield', 'Bart', 'Male', 'Simpsom'),
+	(3, 'Springfield', 'Lizza', 'Female', 'Simpsom'),
+	(4, 'Springfield', 'Milhouse', 'Male', 'Tesla');
