692fb705208dec84049fcd3ceacac9702ee8ae5c
DerecRoss <derec@walle.localdomain>
2026-01-19 14:47:16 -0300

    feat: add support for serialize in XML media type.

---
+++ src/main/java/com/rest_api/spring_boot_rest_api/config/WebConfig.java
@@ -0,0 +1,39 @@
+package com.rest_api.spring_boot_rest_api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration // this class have configurations and possible bean declaration.
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+
+        // extension .xml and .json is deprecated on Spring Boot 2.6
+
+        // with Query Param is the correctly - http://localhost:8080/api/person/v1/2?mediaType=xml
+
+        //        configurer.favorParameter(true)
+        //                .parameterName("mediaType")
+        //                .ignoreAcceptHeader(true) // ignore override parameters in headers of request
+        //                .useRegisteredExtensionsOnly(false)
+        //                .defaultContentType(MediaType.APPLICATION_JSON)
+        //                    .mediaType("json", MediaType.APPLICATION_JSON)
+        //                    .mediaType("xml", MediaType.APPLICATION_XML);
+
+//        configurer.favorParameter(false)
+//                .ignoreAcceptHeader(false) // ignore override parameters in headers of request
+//                .useRegisteredExtensionsOnly(false)
+//                .defaultContentType(MediaType.APPLICATION_JSON)
+//                    .mediaType("json", MediaType.APPLICATION_JSON)
+//                    .mediaType("xml", MediaType.APPLICATION_XML);
+
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false) // ignore override parameters in headers of request
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
+    }
+}
