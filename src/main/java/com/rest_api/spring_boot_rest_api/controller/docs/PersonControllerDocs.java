ea952a8c423ae2367295b2c616ad0894c03a9083
DerecRoss <derec@walle.localdomain>
2026-01-21 15:51:21 -0300

    feat: extract OpenApi annotations to interface.

---
+++ src/main/java/com/rest_api/spring_boot_rest_api/controller/docs/PersonControllerDocs.java
@@ -0,0 +1,77 @@
+package com.rest_api.spring_boot_rest_api.controller.docs;
+
+import com.rest_api.spring_boot_rest_api.dto.v1.PersonDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+public interface PersonControllerDocs {
+    @Operation(summary = "Find Person by ID", description = "Find specific person by ID", tags = {"People"}, responses = {
+            @ApiResponse(description = "Success", responseCode = "200",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = PersonDto.class))),
+
+            @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+    })
+    ResponseEntity<PersonDto> findById(@PathVariable Long id) throws BadRequestException;
+
+    @Operation(summary = "Find all people", description = "Find all people", tags = {"People"}, responses = {
+            @ApiResponse(description = "Success", responseCode = "200", content = {
+                    @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = PersonDto.class))
+                    )
+            }),
+            @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+    })
+    ResponseEntity<List<PersonDto>> findAll();
+
+    @Operation(summary = "Save new Person", description = "Save new Person with Dto", tags = {"People"}, responses = {
+            @ApiResponse(description = "Success", responseCode = "200",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = PersonDto.class))),
+
+            @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+    })
+    ResponseEntity<PersonDto> save(@RequestBody PersonDto person);
+
+    @Operation(summary = "Update exists Person", description = "Update Person in Database", tags = {"People"}, responses = {
+            @ApiResponse(description = "Success", responseCode = "200",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = PersonDto.class))),
+
+            @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+    })
+    ResponseEntity<PersonDto> put(@RequestBody PersonDto person) throws BadRequestException;
+
+    @Operation(summary = "Delete person by ID", description = "Delete exists person by ID", tags = {"People"}, responses = {
+            @ApiResponse(description = "Success", responseCode = "200",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = PersonDto.class))),
+
+            @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+    })
+    ResponseEntity<Void> delete(@PathVariable Long id) throws BadRequestException;
+}
